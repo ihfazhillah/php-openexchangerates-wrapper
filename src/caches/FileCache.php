d7a4646209f5f94d4d7dc0952e6a3525ed85094b
ihfazhillah <mihfazhillah@gmail.com>
2018-03-16 10:27:24 +0700

    add comments in FileCache class

--- src/caches/FileCache.php
+++ src/caches/FileCache.php
@@ -1,5 +1,18 @@
 <?php namespace OpenExchangeRatesWrapper\Caches;
 
+/**
+ * cache handler for OpenExchangeRates
+ *
+ * ```
+ * $cache = new FileCache(1, "/home/yourdir");
+ * $cache->set('foo', 'bar');
+ * $cache->get('foo'); // return 'bar'
+ * $cache->get('bar'); // return false
+ * ```
+ *
+ * @param float $expiredAfter in hour ya, default is 24 (one day)
+ * @param string $cacheDir default is this root directory
+ */
 class FileCache
 {
     protected $defaultDir = __DIR__ . '/../../';
@@ -15,21 +28,42 @@ class FileCache
 
         $this->expiredAfter = $expiredAfter ? $expiredAfter : 24;
     }
+
+    /**
+     * get cachedir
+     * @return string
+     */
     public function getCacheDir(): string
     {
         return $this->cacheDir;
     }
 
+    /**
+     * get expiredAfter in hours
+     *
+     * @return float
+     */
     public function getExpireAfter(): float
     {
         return $this->expiredAfter;
     }
 
+    /**
+     * get expiredAfter in seconds
+     *
+     * @return float
+     */
     public function getExpireAfterSeconds(): float
     {
         return $this->expiredAfter * 60 * 60;
     }
 
+    /**
+     * set the caches directory.
+     * if not found, this will create a new caches directory
+     *
+     * @return void
+     */
     public function setDirectory()
     {
         $this->childCacheDir = $this->cacheDir . "/caches";
@@ -39,6 +73,12 @@ class FileCache
         }
     }
 
+    /**
+     * set the file, create a cache file and return full path to that file
+     *
+     * @param string $name
+     * @return string
+     */
     public function setFile(string $name)
     {
         $this->setDirectory();
@@ -52,6 +92,12 @@ class FileCache
         return $fullname;
     }
 
+    /**
+     * check if timestamp in cache file valid or not
+     *
+     * @param int $timestamp
+     * @return bool
+     */
     public function isValidTime(int $timestamp): bool
     {
         $expiredAt = $timestamp + $this->getExpireAfterSeconds();
@@ -59,11 +105,24 @@ class FileCache
         return time() < $expiredAt;
     }
 
+    /**
+     * get timestamp expiredat the cache
+     *
+     * @return int timestamp
+     */
     public function getExpiredAt(int $timestamp)
     {
         return $timestamp + $this->getExpireAfterSeconds();
     }
 
+    /**
+     * set the value of cache into file
+     *
+     * @param string $key name of the file without .json ext
+     * @param string $value value that you want to cache it
+     *
+     * @return void
+     */
     public function set(string $key, string $value)
     {
 
@@ -83,6 +142,17 @@ class FileCache
         file_put_contents($fullname, json_encode($content));
     }
 
+    /**
+     * get value from cache
+     * return false if:
+     * 1. no content found or file not found
+     * 2. no timestamp key in file
+     * 3. no value key in file
+     * 4. the time is not valid: string or expired
+     *
+     *
+     * @return string | bool
+     */
     public function get(string $key)
     {
         $filename = $this->setFile($key . ".json");
